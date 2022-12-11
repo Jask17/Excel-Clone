e708d2fb23ecbe846e48169438f16d1a442b7681
unknown <jas2023kirat@gmail.com>
2022-12-11 23:51:10 +0530

    Built data-cell-container

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    let cellContainer = $(".input-cell-container");
+    for (let i = 1; i <= 100; i++) {
+        let n = i;
+        let ans = "";
+
+        while (n > 0) {
+            let rem = n % 26;
+            if (rem == 0) {
+                ans = "z" + ans;
+                n = Math.floor(n / 26) - 1;
+            } else {
+                ans = String.fromCharCode(rem - 1 + 65) + ans;
+                n = Math.floor(n / 26);
+            }
+        }
+        let column = $(`<div class="column-name" id="colId-${i}" data="colCode-${ans}">${ans}</div>`);
+        $(".column-name-container").append(column);
+        let row = $(`<div class="row-name" id="rowId-${i}">${i}</div>`);
+        $(".row-name-container").append(row);
+    }
+
+    for (let i = 1; i <= 100; i++) {
+        let row = $(`<div class="input-cell-row"></div>`);
+        for (let j = 1; j <= 100; j++) {
+            // let colCode = $(`.colId-${j}`).attr("id").split("-")[1];
+            let cell = $(`<div class="input-cell" id="rowId-${i}-colId-${j}" contenteditable="true"></div>`);
+            row.append(cell);
+        }
+        $(".input-cell-container").append(row);
+    }
+});
